8ce3af3766f9d29ea3356495c49f56fee01f0761
Jocelynn Hartwig <jhartwig@apterainc.com>
2021-01-28 09:37:02 -0500

    feat: styling and portfolio components

--- src/App.js
+++ src/App.js
@@ -2,35 +2,58 @@ import React, { useEffect } from 'react';
 import ImageLoader from './components/ImageLoader';
 import ImageInput from './components/ImageInput';
 import Home from './components/Home';
+import Blog from './components/Blog';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  nav: {
+    display: "flex",
+    justifyContent: "space-around",
+    paddingTop: "25px",
+    backgroundColor: "#003B73",
+    paddingBottom: "25px"
+  },
+  link: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontFamily: "Courier New",
+    fontSize: "20px"
+  }
+})
 
 const App = () => {
+  const classes = useStyles();
   useEffect(() => {
     document.title = "Mars Image Classifier"
   }, []);
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/curiosity">Curiosity Images</Link>
-            </li>
-            <li>
-              <Link to="/upload-image">Upload an Image</Link>
-            </li>
-          </ul>
+        <nav className={classes.nav}>
+          <div>
+            <Link className={classes.link} to="/">Home</Link>
+          </div>
+          <div>
+            <Link className={classes.link} to="/curiosity">Curiosity Images</Link>
+          </div>
+          <div>
+            <Link className={classes.link} to="/upload-image">Upload an Image</Link>
+          </div>
+          <div>
+            <Link className={classes.link} to="/blog">Blog</Link>
+          </div>
         </nav>
 
         <Switch>
+          <Route path="/blog">
+            <Blog />
+          </Route>
           <Route path="/curiosity">
             <ImageLoader />
           </Route>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    root: {
+        height: "100%",
+        backgroundColor: "#738FA7",
+        margin: "0"
+    },
+    container: {
+        display: "flex",
+        flexGrow: "1",
+        height: "100%"
+    },
+    content: {
+        height: "100vh",
+        color: "#FFFFFF",
+        fontFamily: "Courier New",
+        fontWeight: "bold",
+        padding: "15px"
+    }
+})
+
+
+
 
 const Home = () => {
+    const classes = useStyles();
+
     return (
-        <div>HOME!</div>
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <div className={classes.content}>
+                    <div>Hi, I'm Jocelynn Hartwig</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
